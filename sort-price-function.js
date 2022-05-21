106b79c9ba81e795ac55c57ffddeeb040d29b7a1
Jhonah <buryakone@gmail.com>
2022-05-21 16:02:22 +0700

    оптимизировал строки кода

--- sort-price-function.js
+++ sort-price-function.js
@@ -12,24 +12,29 @@ let courses = [
 ];
 
 // Варианты цен (фильтры), которые ищет пользователь
-let requiredRange1 = [null, 200];
-let requiredRange2 = [100, 350];
-let requiredRange3 = [200, null];
+let requiredRange1       = [null, 200];
+let requiredRange2       = [100, 350];
+let requiredRange3       = [200, null];
+
+/**
+ * Разделяем курсы на три массива, чтобы облегчить обработку
+ */
+// курсы до определенной цены [null, 400]
+let coursesTo            = courses.filter ( ({prices}) => prices[0] === null && prices[1] !== null );
+// курсы от определенной цены [200, null]
+let coursesFrom          = courses.filter ( ({prices}) => prices[0] !== null && prices[1] === null );
+// курсы от и до [100, 200]
+let coursesFromAndTo     = courses.filter ( ({prices}) => prices[0] !== null && prices[1] !== null );
 
 /**
  * Функции для поиска курса до определенного цены [null, 200]
- * @param {[number | null, number | null]} courses - список курсов в виде массива объектов, состоящий из двух параметров
  * @param {number} to - число, до которого осуществляется поиск
  */
-let SortByTo = (courses, to) => {
+let SortByTo = (to) => {
     let selectedCourses  = null;
-
-    let coursesTo        = courses.filter ( ({prices}) => prices[0] === null ); // курсы до определенной цены [null, 400]
-    let coursesFrom      = courses.filter ( ({prices}) => prices[1] === null ); // курсы от определенной цены [200, null]
-    let coursesFromAndTo = courses.filter ( ({prices}) => prices[0] !== null && prices[1] !== null ); // курс от и до [100, 200]
     
-    selectedCourses      = coursesTo.filter( ({prices}) => prices[1] <= to );
-    selectedCourses.push(...coursesFrom.filter( ({prices}) => prices[0] <= to ));
+    selectedCourses      =         coursesTo.filter( ({prices}) => prices[1] <= to );
+    selectedCourses.push(     ...coursesFrom.filter( ({prices}) => prices[0] <= to ));
     selectedCourses.push(...coursesFromAndTo.filter( ({prices}) => prices[0] <= to && prices[1] <= to))
 
     // [подходящие курсы для каждого варианта фильтра]
@@ -38,19 +43,14 @@ let SortByTo = (courses, to) => {
 
 /**
  * Функции для поиска курса в пределах диапозона цен [100, 350]
- * @param {[number | null, number | null]} courses - список курсов в виде массива объектов, состоящий из двух параметров
  * @param {number} from - число, от которого осуществляется поиск
  * @param {number} to - число, до которого осуществляется поиск
  */
-let SortByFromAndTo = (courses, from, to) => {
+let SortByFromAndTo = (from, to) => {
     let selectedCourses  = null;
-
-    let coursesTo        = courses.filter ( ({prices}) => prices[0] === null ); // курсы до определенной цены [null, 400]
-    let coursesFrom      = courses.filter ( ({prices}) => prices[1] === null ); // курсы от определенной цены [200, null]
-    let coursesFromAndTo = courses.filter ( ({prices}) => prices[0] !== null && prices[1] !== null ); // курс от и до [100, 200]
     
-    selectedCourses      = coursesTo.filter( ({prices}) => prices[1] >= from && prices[1] <= to );
-    selectedCourses.push(...coursesFrom.filter( ({prices}) => prices[0] >= from && prices[0] <= to ));
+    selectedCourses      =         coursesTo.filter( ({prices}) => prices[1] >= from && prices[1] <= to );
+    selectedCourses.push(     ...coursesFrom.filter( ({prices}) => prices[0] >= from && prices[0] <= to ));
     selectedCourses.push(...coursesFromAndTo.filter( ({prices}) => prices[0] >= from && prices[1] <= to))
 
     // [подходящие курсы для каждого варианта фильтра]
@@ -59,18 +59,13 @@ let SortByFromAndTo = (courses, from, to) => {
 
 /**
  * Функции для поиска курса от определенной цены [200, null]
- * @param {[number | null, number | null]} courses - список курсов в виде массива объектов, состоящий из двух параметров
  * @param {number} from - число, от которого осуществляется поиск
  */
-let SortByFrom = (courses, from) => {
+let SortByFrom = (from) => {
     let selectedCourses  = null;
 
-    let coursesTo        = courses.filter ( ({prices}) => prices[0] === null ); // курсы до определенной цены [null, 400]
-    let coursesFrom      = courses.filter ( ({prices}) => prices[1] === null ); // курсы от определенной цены [200, null]
-    let coursesFromAndTo = courses.filter ( ({prices}) => prices[0] !== null && prices[1] !== null ); // курс от и до [100, 200]
-    
-    selectedCourses      = coursesTo.filter( ({prices}) => prices[1] >= from );
-    selectedCourses.push(...coursesFrom.filter( ({prices}) => prices[0] >= from ));
+    selectedCourses      =         coursesTo.filter( ({prices}) => prices[1] >= from );
+    selectedCourses.push(     ...coursesFrom.filter( ({prices}) => prices[0] >= from ));
     selectedCourses.push(...coursesFromAndTo.filter( ({prices}) => prices[0] >= from && prices[1] >= from))
 
     // [подходящие курсы для каждого варианта фильтра]
@@ -84,32 +79,32 @@ let SortByFrom = (courses, from) => {
  * Делим его курсы на три варианта: "от", "до", "от и до". Так нам легче будет обработать их отдельно.
  * Тоже самое сделаем  со значениями от пользователя. Обрабатываем каждый из трех вариантов отдельно.
  * 
- * @param {*} requiredRange - Варианты цен (фильтры), которые ищет пользователь
- * @param {*} courses - Список курсов
+ * @param {[number | null, number | null]} requiredRange - Варианты цен (фильтры), которые ищет пользователь
+ * @param {[number | null, number | null]} courses - список курсов в виде массива объектов, состоящий из двух параметров
  */
 let SortByPrice = (requiredRange, courses) => {
-    const [from, to]     = requiredRange;
+    const                          [from, to] = requiredRange;
 
     const RESULT = "" + requiredRange.reduce( (prev, curr) => { 
-        let a = 0;
-        if (prev === null) a = 1;
-        if (curr === null) a = 2;
-        if (prev !== null && curr !== null) a =3;
+        let                                 a = 0;
+        if (prev === null)                  a = 1;
+        if (curr === null)                  a = 2;
+        if (prev !== null && curr !== null) a = 3;
         return a;
     });
     
     switch(RESULT) {
         case "1":
             alert("to");
-            SortByTo(courses, to);            
+            SortByTo(to);            
             break;
         case "2":
             alert("from");
-            SortByFrom(courses, from);
+            SortByFrom(from);
             break;
         case "3":
             alert("from to");
-            SortByFromAndTo(courses, from, to);
+            SortByFromAndTo(from, to);
             break;
         default:            
             break;
